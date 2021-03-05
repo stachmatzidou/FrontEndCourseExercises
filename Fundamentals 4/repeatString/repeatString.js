541a5570515b44ca725cb2df9e3f72fa48261ae9
stachmatzidou <stachmatzidou@gmail.com>
2021-03-05 11:38:42 +0200

    repeatString exercise done!

--- Fundamentals 4/repeatString/repeatString.js
+++ Fundamentals 4/repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times<0) {
+        return "ERROR";
+    }
+    let output = "";
+    for ( let i = 0; i < times; i++) {
+        output += string;
+    }
+    return output;
 }
+
 
 module.exports = repeatString
