91d678ce784a8d4a088a7d8eeac70750074ead97
stachmatzidou <stachmatzidou@gmail.com>
2021-03-26 19:27:12 +0200

    All done

--- Fundamentals 5/palindromes/palindromes.js
+++ Fundamentals 5/palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
+const palindromes = function (string) {
+    const justLetters = string.split("").filter(function (letter) {
+        if (letter.charCodeAt() >= 65 && letter.charCodeAt() <= 90 || letter.charCodeAt() >= 97 && letter.charCodeAt() <= 122) {
+            return true;
+        }
+    });
+    const normalString = justLetters.join("").toLowerCase();
+    const reversedString = justLetters.reverse().join("").toLowerCase();
 
+    if (normalString === reversedString) {
+        return true;
+    }
+    return false;
 }
 
 module.exports = palindromes
